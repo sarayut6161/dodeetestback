f56bafcd6cfc3a37145328a63a036276cbf68489
Sarayut Rascharit <mju6204106323@mju.ac.th>
2023-10-24 16:57:53 +0700

    commit1

--- src/main/java/com/example/dodeetestback/FibonacciController.java
+++ src/main/java/com/example/dodeetestback/FibonacciController.java
@@ -1,2 +1,35 @@
-package com.example.dodeetestback;public class FibonacciController {
+package com.example.dodeetestback;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class FibonacciController {
+
+    @GetMapping("/fibonacci")
+    public FibonacciResponse calculateFibonacci(
+            @RequestParam(name = "member-count") int memberCount
+    ) {
+        if (memberCount < 1 || memberCount > 100) {
+            throw new IllegalArgumentException("Invalid member count. It should be between 1 and 100.");
+        }
+
+        List<Integer> sequence = new ArrayList<>();
+        int a = 0, b = 1;
+        int total = 0;
+
+        for (int i = 0; i < memberCount; i++) {
+            sequence.add(a);
+            total += a;
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return new FibonacciResponse(memberCount,sequence, total); // รวมคุณสมบัติ memberCount
+    }
 }
--- src/main/java/com/example/dodeetestback/FibonacciResponse.java
+++ src/main/java/com/example/dodeetestback/FibonacciResponse.java
@@ -1,2 +1,27 @@
-package com.example.dodeetestback;public class FibonacciResponse {
+package com.example.dodeetestback;
+
+import java.util.List;
+
+public class FibonacciResponse {
+    private int memberCount; // ย้ายคุณสมบัติ memberCount ไปด้านบน
+    private List<Integer> sequence;
+    private int total;
+
+    public FibonacciResponse(int memberCount, List<Integer> sequence, int total) {
+        this.memberCount = memberCount;
+        this.sequence = sequence;
+        this.total = total;
+    }
+
+    public int getMemberCount() {
+        return memberCount;
+    }
+
+    public List<Integer> getSequence() {
+        return sequence;
+    }
+
+    public int getTotal() {
+        return total;
+    }
 }
