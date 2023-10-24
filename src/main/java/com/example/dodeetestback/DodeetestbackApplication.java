146368bfd8cb42d7ab48e3b4dff362e9d7378fe4
Sarayut Rascharit <mju6204106323@mju.ac.th>
2023-10-24 16:26:28 +0700

    first commit

---
+++ src/main/java/com/example/dodeetestback/DodeetestbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dodeetestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodeetestbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodeetestbackApplication.class, args);
+    }
+
+}
